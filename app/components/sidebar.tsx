09efbf1caefc5848610d017442645fb592580286
mlusevi <135083106+mlusevi@users.noreply.github.com>
2023-05-31 10:18:05 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGpt Web</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Deployed by cc.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
